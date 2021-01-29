ca4d03af26c39144cbabe9e7356bc0aa48e00281
Veda Narayana Koraganji <vkoragan@iu.edu>
2021-01-29 18:12:29 -0500

    Auto stash before rebase of "origin/main"

--- additional funtions/eazyPlot.m
+++ additional funtions/eazyPlot.m
@@ -1,5 +1,10 @@
+<<<<<<< Updated upstream
 function eazyPlot(xData, yData, xLabel, yLabel, Title)
 
 plot(xData, yData)
 
+=======
+function eazyPlot(xData, yData, Title, xLabel, yLabel)
+
+>>>>>>> Stashed changes
 end
